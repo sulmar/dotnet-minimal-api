0d6a04ea0e758ed5d4f45ef2c988a3393101d5ca
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-05-14 11:03:08 +0200

    Zamiana na słownik

--- FakeRepositories/FakeCustomerRepository.cs
+++ FakeRepositories/FakeCustomerRepository.cs
@@ -1,51 +1,44 @@
 ﻿using Bogus;
 using minimal_api.IRepositories;
 using minimal_api.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace minimal_api.FakeRepositories
 {
     public class FakeCustomerRepository : ICustomerRepository
     {
-        private ICollection<Customer> customers;
-
-        private readonly Faker<Customer> faker;
+        private IDictionary<int, Customer> customers;
 
         public FakeCustomerRepository(Faker<Customer> faker)
         {
-            this.faker = faker;
-
-            customers = faker.Generate(100);
+            customers = faker.Generate(100).ToDictionary(p=>p.Id);
         }
 
-        public void Add(Customer customer)
-        {            
-            customers.Add(customer);
-        }
+        public void Add(Customer customer) => customers.Add(customer.Id, customer);
 
-        public IEnumerable<Customer> Get()
-        {
-            return customers;
-        }
+        public IEnumerable<Customer> Get() => customers.Values;
 
         public Customer Get(int id)
         {
-            return customers.SingleOrDefault(c => c.Id == id);
+            if (customers.TryGetValue(id, out var entity))
+            {
+                return entity;
+            }
+            else
+            {
+                return null;
+            }
         }
 
-        public void Remove(int id)
-        {
-            customers.Remove(Get(id));
-        }
+        public void Remove(int id) => customers.Remove(id);
 
         public void Update(Customer customer)
         {
-            throw new NotImplementedException();
+            Remove(customer.Id);
+            Add(customer);
         }
 
-        public bool IsExists(int id) => customers.Any(c => c.Id == id);
+        public bool IsExists(int id) => customers.ContainsKey(id);
     }
 }
